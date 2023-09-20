916fc13cb6e271edfcaa4199cd6b7c03c6df0a79
caionikolas <Caio.kanu@hotmail.com>
2023-09-20 00:16:10 -0300

    Project Start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='bg-gray-800'>
+      <h1 className='text-gray-200'>Ola Mundo</h1>
+    </div>
+  )
+}
